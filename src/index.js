7885735e043a53aabc48338b041e236ea87dca34
danielahn <93danielahn@gmail.com>
2019-09-17 17:54:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.querySelector("#create-task-form").addEventListener("submit", function(event){
+//         event.preventDefault()
+//         const submission = document.querySelector('#new-task-description').value
+//         document.querySelector("#tasks").insertAdjacentHTML(`<li>${submission}</li>`)
+//     });
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.querySelector("#create-task-form").addEventListener("submit", addToList);
+//     // we need to put in a reference to a function.. NOT INVOKING IT
+// });
+
+
+// function addToList(event) {
+//     event.preventDefault()
+//     const submission = document.querySelector('#new-task-description').value
+//     document.querySelector("#tasks").insertAdjacentHTML("beforeend", `<li>${submission} <button data-description=${submission}> x </button></li>`)
+// }
