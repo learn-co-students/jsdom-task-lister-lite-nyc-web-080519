530a032e55448b75e56c74cf8aeaec6ee5973501
Andy Choi <wooseokchoi96@gmail.com>
2019-09-19 17:08:09 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const newForm = document.querySelector("#create-task-form");
+  const textBox = document.querySelector("#new-task-description");
+  const tasks = document.querySelector("#tasks");
+
+  newForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const input = textBox.value;
+    tasks.insertAdjacentHTML("beforeend", 
+      `<li>
+          ${input}
+          <button>X</button>
+      </li>`
+    );
+    newForm.reset()
+  });
+
+  tasks.addEventListener("click", e => {
+    if(e.target.tagName === "BUTTON"){
+      e.target.parentElement.remove();
+    }
+  });
+
+}); 
